805aa1f50e1215b907e1ce85f17e13f8c9055044
Nicolas Hennick <nicolashennick@gmail.com>
2019-01-02 11:20:32 +0000

    initial commit

---
+++ spec/controllers/people_controller_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe PeopleController do
+  describe "#create" do
+    context "when person is valid" do
+      it "redirects to #show" do
+        pending "create this test"
+      end
+    end
+
+    context "when person is invalid" do
+      it "renders the 'new' template" do
+        pending "create this test"
+      end
+    end
+  end
+end
